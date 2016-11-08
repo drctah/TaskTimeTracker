945af7545d82507b8ccbb73c705455750705caeb
Wr4thon <johannes@thegametender.de>
2016-11-08 13:07:48 +0100

    cleanup after merge

--- TaskTimeTracker/Task.cs
+++ TaskTimeTracker/Task.cs
@@ -4,6 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
 using TaskTimeTracker.Client.Contract;
+using TaskTimeTracker.Client.Ui.Commands;
 
 namespace TaskTimeTracker.Client {
   public class Task : INotifyPropertyChanged, ITask {
